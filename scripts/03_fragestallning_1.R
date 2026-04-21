06850aa889e2d61970bab3a1132af648a7440ddd
Alma Vasell <alma.vasell@hotmail.com>
2026-04-21 10:36:15 +0200

    ändrade order_value till order_value_net

--- scripts/03_fragestallning_1.R
+++ scripts/03_fragestallning_1.R
@@ -1,24 +1,25 @@
 source("scripts/01_load_data.R")
 source("scripts/02_clean_data.R")
+
 library(tidyverse)
 
-data_analysis <- data_clean %>%
-  mutate(order_value = quantity * unit_price * (1 - discount_pct))
+data_analysis <- data_clean
 
 sales_by_category <- data_analysis %>%
   group_by(product_category) %>%
   summarise(
-    total_sales = sum(order_value, na.rm = TRUE),
-    avg_sales = mean(order_value, na.rm = TRUE),
-    median_sales = median(order_value, na.rm = TRUE),
+    total_sales = sum(order_value_net, na.rm = TRUE),
+    avg_sales = mean(order_value_net, na.rm = TRUE),
+    median_sales = median(order_value_net, na.rm = TRUE),
     n_orders = n()
   ) %>%
   arrange(desc(total_sales))
 
 sales_by_category
 
-# Visualiseringar
-  ggplot(sales_by_category, aes(x = reorder(product_category, total_sales), y = total_sales)) +
+# Visualisering 1: Total försäljning
+
+ggplot(sales_by_category, aes(x = reorder(product_category, total_sales), y = total_sales)) +
   geom_col(fill = "#0F4C5C") +
   coord_flip() +
   labs(
@@ -27,12 +28,14 @@ sales_by_category
     y = "Total försäljning"
   )
 
-  ggplot(sales_by_category, aes(x = reorder(product_category, n_orders), y = n_orders)) +
-    geom_col(fill = "#0F4C5C") +
-    coord_flip() +
-    labs(
-      title = "Antal ordrar per produktkategori",
-      x = "Produktkategori",
-      y = "Antal ordrar"
-    )
-  
+
+# Visualisering 2: Antal ordrar
+
+ggplot(sales_by_category, aes(x = reorder(product_category, n_orders), y = n_orders)) +
+  geom_col(fill = "#0F4C5C") +
+  coord_flip() +
+  labs(
+    title = "Antal ordrar per produktkategori",
+    x = "Produktkategori",
+    y = "Antal ordrar"
+  )
